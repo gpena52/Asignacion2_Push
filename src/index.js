b25b2a2fbc25a40e2c514f3d9cd85e6d1d88f6d7
Gabriel <gabrielpena0209@gmail.com>
2026-05-14 20:17:20 -0400

    Desarrollo del body

--- src/index.js
+++ src/index.js
@@ -1,22 +1,97 @@
 import * as readline from "node:readline";
+import { mkdir, writeFile } from 'node:fs/promises';
 import { stdin as input, stdout as output } from "node:process";
+import validator from 'validator';
 const rl = readline.createInterface({ input, output });
-function ask(question) {
-    return new Promise((resolve) => {
-        rl.question(question, (answer) => {
-            resolve(answer);
+var TipoCuenta;
+(function (TipoCuenta) {
+    TipoCuenta["CC"] = "CC";
+    TipoCuenta["CA"] = "CA";
+})(TipoCuenta || (TipoCuenta = {}));
+var Validacion;
+(function (Validacion) {
+    Validacion[Validacion["String"] = 0] = "String";
+    Validacion[Validacion["Number"] = 1] = "Number";
+    Validacion[Validacion["Email"] = 2] = "Email";
+    Validacion[Validacion["TipoCuenta"] = 3] = "TipoCuenta";
+})(Validacion || (Validacion = {}));
+async function ask(question, type) {
+    return new Promise(async (resolve) => {
+        rl.question(question, async (answer) => {
+            let isValid = true;
+            switch (type) {
+                case Validacion.String:
+                    isValid = !!answer.trim();
+                    break;
+                case Validacion.Number:
+                    isValid = !isNaN(Number(answer)) && Number(answer) > 0;
+                    break;
+                case Validacion.Email:
+                    isValid = answer.trim() == "" || validator.isEmail(answer);
+                    break;
+                case Validacion.TipoCuenta:
+                    answer = answer.toUpperCase();
+                    isValid = Object.values(TipoCuenta).includes(answer);
+                    break;
+            }
+            if (isValid) {
+                resolve(answer);
+            }
+            else {
+                resolve(await ask(question, type));
+            }
         });
-        // rl.close();
     });
 }
+async function createFile(data) {
+    try {
+        await mkdir('./archivos', { recursive: true });
+        let texto = "";
+        for (const empleado of data) {
+            texto += Object.values(empleado).map(v => (!!v) ? v : "N/A").join(",");
+            await writeFile(`archivos/${empleado.cedula}.txt`, texto);
+            texto = "";
+        }
+    }
+    catch (error) {
+        console.error('Error creando el archivo:', error);
+    }
+}
 async function main() {
-    const cedula = await ask("Cedula: ");
-    const numeroCuentaEmisor = await ask("Numero cuenta del emisor: ");
-    const numeroCuentaEmpleado = await ask("Numero cuenta del empleado: ");
-    const tipoCuenta = await ask("Tipo de cuenta: ");
-    const monto = Number(await ask("Monto: "));
-    const correoEmpleado = await ask("Correo del empleado: ");
-    console.log({ cedula, numeroCuentaEmisor, numeroCuentaEmpleado, tipoCuenta, monto, correoEmpleado });
+    let activo = true;
+    let data = [];
+    while (activo) {
+        console.log("\n------------------------------------------\n");
+        console.log("1. Ingresar un nuevo empleado");
+        console.log("2. Salir");
+        let opcion = await ask("\nElija una opcion: ", Validacion.String);
+        switch (opcion.trim()) {
+            case "1":
+                const cedula = await ask("Cedula: ", Validacion.String);
+                const numeroCuentaEmisor = await ask("Numero cuenta del emisor: ", Validacion.String);
+                const numeroCuentaEmpleado = await ask("Numero cuenta del empleado: ", Validacion.String);
+                const tipoCuenta = await ask("Tipo de cuenta(CC, CA): ", Validacion.TipoCuenta);
+                const monto = Number(await ask("Monto: ", Validacion.Number));
+                const correoEmpleado = await ask("Correo del empleado (Opcional): ", Validacion.Email);
+                data.push({
+                    cedula,
+                    numeroCuentaEmisor,
+                    numeroCuentaEmpleado,
+                    tipoCuenta,
+                    monto,
+                    correoEmpleado
+                });
+                break;
+            case "2":
+                await createFile(data);
+                activo = false;
+                break;
+            default:
+                console.log("Opcion no valida");
+                break;
+        }
+    }
+    rl.close();
 }
 main().catch(console.error);
 //# sourceMappingURL=index.js.map
